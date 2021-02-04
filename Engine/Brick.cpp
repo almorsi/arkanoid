376cc3f60a39366a7c23c79cb93830898b011dc0
Ammar Nasser <ammarnasser0012@gmail.com>
2021-02-04 23:02:17 +0200

    improve the collision with ball function in brick.cpp

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -20,55 +20,11 @@ void Brick::draw(Graphics& gfx) const
 
 bool Brick::is_collide_with_ball(Ball& ball)
 {
-	// ________________  
-	// |	2  |	1  |
-	// |_ _ _ _|_ _ _ _|  DIVIDE THE RECTANGLE INTO FOUR RECTAGNLES TO SOLVE THE PHYSICS BUG
-	// |	3  |	4  |
-	// |______ |_______|
-	if (!destroyed)
+	if (!destroyed && ball.get_rectangle().is_overlapping_with(rect))
 	{
-		using am::Rectangle;
-		const Rectangle rect_1 = Rectangle(Vec2(center.x, center.y - half_height), Vec2(center.x + half_width, center.y));
-		const Rectangle rect_2 = Rectangle(Vec2(center.x - half_width, center.y - half_height), Vec2(center.x, center.y));
-		const Rectangle rect_3 = Rectangle(Vec2(center.x - half_width, center.y), Vec2(center.x , center.y + half_height));
-		const Rectangle rect_4 = Rectangle(Vec2(center.x, center.y), Vec2(center.x + half_width, center.y + half_height));
-		Vec2 vel = ball.get_velocity();
-		if (ball.get_rectangle().is_overlapping_with(rect_1))
-		{
-			if (vel.x < 0)
-				ball.rebound_x();
-			if (vel.y > 0)
-				ball.rebound_y();
-			destroyed = true;
-			return true;
-		}
-		else if (ball.get_rectangle().is_overlapping_with(rect_2))
-		{
-			if (vel.x > 0)
-				ball.rebound_x();
-			if (vel.y > 0)
-				ball.rebound_y();
-			destroyed = true;
-			return true;
-		}
-		else if (ball.get_rectangle().is_overlapping_with(rect_3))
-		{
-			if (vel.x > 0)
-				ball.rebound_x();
-			if (vel.y < 0)
-				ball.rebound_y();
-			destroyed = true;
-			return true;
-		}
-		else if (ball.get_rectangle().is_overlapping_with(rect_4))
-		{
-			if (vel.x < 0)
-				ball.rebound_x();
-			if (vel.y < 0)
-				ball.rebound_y();
-			destroyed = true;
-			return true;
-		}
+		ball.reset_vel_after_pad_collision(Vec2(ball.get_pos() - center));
+		destroyed = true;
+		return true;
 	}
 	return false;
 }
